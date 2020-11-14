ecaaf273e2b7f20425043e429d2090ee0edd4942
Hanifan Rizki Nurahman <46918200+rightfel@users.noreply.github.com>
2020-11-14 17:02:09 +0700

    Create is a number is prime.py

---
+++ 6 kyu/is a number is prime.py
@@ -0,0 +1,31 @@
+'''
+Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Requirements
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+Example
+is_prime(1)  /* false */
+is_prime(2)  /* true  */
+is_prime(-1) /* false */
+'''
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    if num < 4:
+        return True
+    # This is checked so that we can skip  
+    # middle five numbers in below loop 
+    if (num % 2 == 0 or num % 3 == 0):
+        return False
+    i = 5
+    while (i * i < num + 1):
+        if (num % i == 0 or num % (i + 2) == 0):
+            return False
+        i += 6
+    return True
