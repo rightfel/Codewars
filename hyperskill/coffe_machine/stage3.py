b3903caa8aaad59c9eb6b1ccf262c254c6db9cc2
Hanifan Rizki Nurahman <46918200+rightfel@users.noreply.github.com>
2020-12-18 11:56:21 +0700

    Create stage3.py

---
+++ hyperskill/coffe_machine/stage3.py
@@ -0,0 +1,6 @@
+# Write your code here
+cups = int(input("Write how many cups of coffee you will need:"))
+print("For", cups, "cups of coffee you will need:")
+print(200 * cups, "ml of water")
+print(50 * cups, "ml of milk")
+print(15 * cups, "g of coffee beans")
