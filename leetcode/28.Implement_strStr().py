7390a0c3cb816a7024d43c7ee52024a13213bee2
Hanifan Rizki Nurahman <46918200+rightfel@users.noreply.github.com>
2021-01-16 09:08:35 +0700

    Create 28.Implement_strStr().py

---
+++ leetcode/28.Implement_strStr().py
@@ -0,0 +1,25 @@
+'''
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+Constraints:
+
+0 <= haystack.length, needle.length <= 5 * 104
+haystack and needle consist of only lower-case English characters.
+
+'''
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
